b2cefa4e55350a4348faea9b9001cc2e8287526c
Shane Lett <shanealexlett@gmail.com>
2021-09-22 14:37:16 -0400

    Update script.js

--- client/flappy/script.js
+++ client/flappy/script.js
@@ -32,12 +32,25 @@ document.addEventListener('DOMContentLoaded' , () => {
 
     function generateObstacle() {
         let obstacleLeft = 500
-        let obstacleBottom = 150
+        let randomHeight = Math.random() * 60
+        let obstacleBottom = randomHeight
         const obstacle = document.createElement('div')
         obstacle.classList.add('obstacle')
         gameDisplay.appendChild(obstacle)
         obstacle.style.left = obstacleLeft + 'px'
         obstacle.style.bottom = obstacleBottom + 'px'
+
+        function moveObstacle() {
+            obstacleLeft -= 2
+            obstacle.style.left = obstacleLeft + 'px'
+
+            if (obstacleLeft === -60) {
+                clearInterval(timerId)
+                gameDisplay.removeChild(obstacle)
+            }
+        }
+        let timerId = setInterval(moveObstacle, 20)
+
     }
     generateObstacle()
 
