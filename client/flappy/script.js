9f8cfe61c9c5d523cdd28b4d9dfc6ad7aadcd106
Shane Lett <shanealexlett@gmail.com>
2021-09-22 14:25:37 -0400

    Update script.js

--- client/flappy/script.js
+++ client/flappy/script.js
@@ -15,6 +15,12 @@ document.addEventListener('DOMContentLoaded' , () => {
     }
     let timerId = setInterval(startGame, 20)
 
+    function control(e) {
+        if (e.keyCode === 32) {
+            jump()
+        }
+    }
+
 
     function jump() {
         if (birdBottom < 500) birdBottom += 50
@@ -22,4 +28,18 @@ document.addEventListener('DOMContentLoaded' , () => {
         console.log(birdBottom)
     }
     document.addEventListener('keyup', jump)
+
+
+    function generateObstacle() {
+        let obstacleLeft = 500
+        let obstacleBottom = 150
+        const obstacle = document.createElement('div')
+        obstacle.classList.add('obstacle')
+        gameDisplay.appendChild(obstacle)
+        obstacle.style.left = obstacleLeft + 'px'
+        obstacle.style.bottom = obstacleBottom + 'px'
+    }
+    generateObstacle()
+
+
 })
