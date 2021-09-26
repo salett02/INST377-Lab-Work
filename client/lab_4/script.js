3f4be995ce978d85e2edf9147c05e4be11917c59
Shane Lett <shanealexlett@gmail.com>
2021-09-26 16:43:20 -0400

    Update script.js

---
+++ client/lab_4/script.js
@@ -0,0 +1,45 @@
+let slidePosition = 0;
+const slides = document.getElementsByClassName('photo-grid-item');
+
+const totalSlides = slides.length;
+
+console.log(slides)
+
+document.
+    getElementById('carousel-button-next')
+    .addEventListener('click', function() {
+        moveToNextSlide();
+    })
+
+document.
+    getElementById('carousel-button-prev')
+    .addEventListener('click', function() {
+        moveToPrevSlide();
+    })
+
+function updateSlidePosition() {
+    for(let slide of slides) {
+        slide.classList.remove('photo-grid-item--visible')
+        slide.classList.add('photo-grid-item--hidden')
+    }
+
+    slides[slidePosition].classList.add('photo-grid-item--visible')
+}
+
+function moveToNextSlide() {
+    if (slidePosition == totalSlides - 1) {
+        slidePosition = 0
+    } else {
+        slidePosition++;
+    }
+    updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+    if (slidePosition == 0) {
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+    updateSlidePosition();
+}
