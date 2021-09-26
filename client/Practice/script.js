6f72b957507b58e75f14eea5a4264fa8545bbaea
Shane Lett <shanealexlett@gmail.com>
2021-09-26 16:37:25 -0400

    Update script.js

--- client/Practice/script.js
+++ client/Practice/script.js
@@ -37,7 +37,7 @@ function moveToNextSlide() {
 
 function moveToPrevSlide() {
     if (slidePosition == 0) {
-        slidePosition = 0
+        slidePosition = totalSlides - 1;
     } else {
         slidePosition--;
     }
