d74306b017dd397b450c891ca89cc415398cafe8
Shane Lett <shanealexlett@gmail.com>
2021-09-26 23:43:32 -0400

    Update script.js

--- client/Practice/script.js
+++ client/Practice/script.js
@@ -1,18 +1,16 @@
 let slidePosition = 0;
-const slides = document.getElementsByClassName('photo-grid-item');
+const slides = document.querySelectorAll('photo-grid-item');
 
 const totalSlides = slides.length;
 
 console.log(slides)
 
-document.
-    getElementById('carousel-button-next')
+document.querySelector('carousel-button-next')
     .addEventListener('click', function() {
         moveToNextSlide();
     })
 
-document.
-    getElementById('carousel-button-prev')
+    document.querySelector('carousel-button-prev')
     .addEventListener('click', function() {
         moveToPrevSlide();
     })
