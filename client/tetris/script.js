df6d39159edef5b58ce7d4e3117cf353c28c14db
Shane Lett <shanealexlett@gmail.com>
2021-10-06 16:42:41 -0400

    Update script.js

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -1,3 +1,57 @@
 document.addEventListener('DOMContentLoaded', () => {
+const grid = document.querySelector('.grid')
+let squares = Array.from(document.querySelectorAll('.grid div'))
+const ScoreDisplay = document.querySelector('#score')
+const StartBtn = document.querySelector('#start-button')
+const width = 10
+
+//Tetris Shapes
+const Tetromino1 = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
+
+  const Tetromino2 = [
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1],
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1]
+  ]
+
+  const Tetromino3 = [
+    [1,width,width+1,width+2],
+    [1,width+1,width+2,width*2+1],
+    [width,width+1,width+2,width*2+1],
+    [1,width,width+1,width*2+1]
+  ]
+
+  const Tetromino4 = [
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1]
+  ]
+
+  const Tetromino5 = [
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3],
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3]
+  ]
+
+  const theTetrominoes = [Tetromino1, Tetromino2, Tetromino3, Tetromino4, Tetromino5]
+
+  let currentPosition = 4
+  let current = theTetrominoes[0][0]
+
+  function draw() {
+      current.forEach(index => {
+          squares[currentPosition + index].classList.add('tetromino')
+      })
+  }
+
+  draw()
 
 })
