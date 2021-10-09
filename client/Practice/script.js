ef18f6cb716d7eb5246ebc568698c2895aac177d
Shane Lett <shanealexlett@gmail.com>
2021-10-08 20:55:43 -0400

    Update script.js

--- client/Practice/script.js
+++ client/Practice/script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')
   let squares = Array.from(document.querySelectorAll('.grid div'))
-  const ScoreDisplay = document.querySelector('#score')
-  const StartBtn = document.querySelector('#start-button')
+  const scoreDisplay = document.querySelector('#score')
+  const startBtn = document.querySelector('#start-button')
   const width = 10
   let nextRandom = 0
 
